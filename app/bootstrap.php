ee2df40320619d2d6912a98a4ff821b8fbc4d3bc
Daniel Wolf <danielrwolf5@gmail.com>
2015-03-05 23:40:29 -0600

    removing features by default

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -5,34 +5,35 @@ require_once dirname(__DIR__) . '/vendor/autoload.php';
 $app = new Silex\Application();
 $app['env'] = getenv('APPLICATION_ENV');
 
-// config
+// Config
 $envConfigFileLoc = __DIR__ . '/config/' . $app['env'] . '.yml';
 $defaultConfigFileLoc = __DIR__ . '/config/default.yml';
 $configFileLoc = file_exists($envConfigFileLoc)? $envConfigFileLoc: $defaultConfigFileLoc;
 $app->register(new DerAlex\Silex\YamlConfigServiceProvider($configFileLoc));
 
-// debug mode
+// Debug mode
 $app['debug'] = $app['config']['debug'];
 
-// database
+// Database
 $app->register(new Silex\Provider\DoctrineServiceProvider(), [
     'db.options' => $app['config']['database']
 ]);
 
-$app->register(new Silex\Provider\HttpCacheServiceProvider(), array(
-    'http_cache.cache_dir' => dirname(__DIR__) . '/cache/',
-));
+// Cache
+//$app->register(new Silex\Provider\HttpCacheServiceProvider(), array(
+//    'http_cache.cache_dir' => dirname(__DIR__) . '/cache/',
+//));
 
-// twig is not always necessary
+// Templates
 //$app->register(new Silex\Provider\TwigServiceProvider(), [
 //    'twig.path' => dirname(__DIR__) . '/frontend/twig',
 //]);
 
-// console app
-$app->register(new Knp\Provider\ConsoleServiceProvider(), array(
-    'console.name'              => $app['config']['name'],
-    'console.version'           => $app['config']['version'],
-    'console.project_directory' => dirname(__DIR__)
-));
+// Console Commands
+//$app->register(new Knp\Provider\ConsoleServiceProvider(), array(
+//    'console.name'              => $app['config']['name'],
+//    'console.version'           => $app['config']['version'],
+//    'console.project_directory' => dirname(__DIR__)
+//));
 
 return $app;
--- public_html/index.php
+++ public_html/index.php
@@ -1,8 +1,8 @@
 <?php
 
-$app = require_once __DIR__ . '/../app/app.php';
+$app = require_once dirname(__DIR__) . '/app/app.php';
 
 // enable cached responses when Cache-Control header set by response
-$app['http_cache']->run();
+//$app['http_cache']->run();
 
-//$app->run();
+$app->run();
